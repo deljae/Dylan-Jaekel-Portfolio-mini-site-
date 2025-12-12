ac5e7485905cb42e36add52e8df6a919985d7582
Dylan Jaekel <DELjaekel@yahoo.com>
2025-12-11 20:06:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.classList.toggle("show");
   });
 });
+const grid= document.querySelector(".photo-grid"); 
+const images = Array.from(grid.children); 
+
+for(let i= images.length - 1; i > 0; i--) 
+{
+  const j = Math.floor(Math.random() * (i + 1)); 
+  [images[i], images[j]] = images[j], images[i]]; 
+}
+images.forEach(img => grid appendChild(img)); 
+
+const lightbox= document.getElementById("lightbox");
+
+Images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display= "flex"; 
+    lightbox.innerHTML= <img
+    src="${img.src}" alt="">;
+  });
+});
+lightbox.addEventListener("click", () => {
+  lightbox.style.display= "none"; 
+});
+});
